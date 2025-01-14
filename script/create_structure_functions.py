ff9550857d0845046705956fd67c2f87188bd034
Yohei Onuki <onuki@riam.kyushu-u.ac.jp>
2025-01-14 14:56:57 +0900

    Update create_structure_functions.py

--- script/create_structure_functions.py
+++ script/create_structure_functions.py
@@ -12,45 +12,34 @@ levels = 64
 eps_i = 1.e-10j
 
 
-def Bessel_I(nu, z, N):
+def Bessel_I(mu, z, N):
     I = 1.e0
     eps = np.sign(np.real(z)) * eps_i
     for m in range(1, N):
         a = 1
         for j in range(1, m+1):
-            a = a * (z + eps)**2 / (4 * j * (j + nu))            
+            a = a * (z + eps)**2 / (4 * j * (j + mu))            
         I = I + a
-    I = (z / 2 + eps)**nu / gamma(nu + 1) * I
+    I = (z / 2 + eps)**mu / gamma(mu + 1) * I
     return I
 
 
-def II(nu, z1, z2, N):
-    return (Bessel_I(nu, z1, N)
-            * Bessel_I(-nu, z2, N)
-            - Bessel_I(-nu, z1, N)
-            * Bessel_I(nu, z2, N))
+def II(mu, z1, z2, N):
+    return (Bessel_I(mu, z1, N)
+            * Bessel_I(-mu, z2, N)
+            - Bessel_I(-mu, z1, N)
+            * Bessel_I(mu, z2, N))
 
 
-def D_func(U0, U1, k, omega, N):
-    nu = 1j * (1 / (U1 - U0)**2 - 0.25)**0.5
-    Z0 = (k * U0 - omega) / (U1 - U0)
-    Z1 = (k * U1 - omega) / (U1 - U0)
-    return np.sqrt(Z1 * Z0 + eps_i) * (Bessel_I(- nu, Z1, N)
-            * Bessel_I(nu, Z0, N)
-            - Bessel_I(nu, Z1, N)
-            * Bessel_I(-nu, Z0, N))
-
-
-def structure_function(U0, U1, k, omega, z, N):
-    nu = 1j * (1 / (U1 - U0)**2 - 0.25)**0.5
+def structure_function(nu, U0, U1, k, omega, z, N):
     Z1 = (k * U1 - omega) / (U1 - U0)
     U = U1 * z + U0 *(1 - z)
     Z = (k * U - omega) / (U1 - U0)
-    return ((Z + eps_i)**0.5 * II(nu, Z1, Z, N))
+    return ((Z + eps_i)**0.5 * II(1j * nu, Z1, Z, N))
 
 
-def structure_function_normal(U0, U1, k, omega, z, N):
-    F = structure_function(U0, U1, k, omega, z, N)
+def structure_function_normal(nu, U0, U1, k, omega, z, N):
+    F = structure_function(nu, U0, U1, k, omega, z, N)
     return F / np.abs(F).max()
 
 
@@ -58,7 +47,7 @@ buoyancy_frequency = 1.
 U0 = 0
 U1 = 0.15
 Ri = buoyancy_frequency / (U0 - U1)**2
-nu = 1j * (Ri - 0.25)**0.5
+nu = (Ri - 0.25)**0.5
 
 N = 100
 
@@ -69,7 +58,7 @@ wave_length = 2 * np.pi / np.abs(k)
 x_max = 4 * wave_length
 z = np.linspace(0, 1, 1000)
 x = np.linspace(0, x_max, 200)
-psi_1d = structure_function_normal(U0, U1, k, omega, z, N)
+psi_1d = structure_function_normal(nu, U0, U1, k, omega, z, N)
 psi =  np.real(np.outer(psi_1d, np.exp(1j * k * x)))
 ax1 = fig.add_subplot(131)
 ax1.set_title(r"$k={:.1f}, \ \sigma={:.1f}$".format(k, omega),
@@ -83,7 +72,7 @@ wave_length = 2 * np.pi / np.abs(k)
 x_max = 4 * wave_length
 z = np.linspace(0, 1, 1000)
 x = np.linspace(0, x_max, 200)
-psi_1d = structure_function_normal(U0, U1, k, omega, z, N)
+psi_1d = structure_function_normal(nu, U0, U1, k, omega, z, N)
 psi =  np.real(np.outer(psi_1d, np.exp(1j * k * x)))
 ax2 = fig.add_subplot(132)
 ax2.set_title(r"$k={:.1f}, \ \sigma={:.1f}$".format(k, omega),
@@ -97,7 +86,7 @@ wave_length = 2 * np.pi / np.abs(k)
 x_max = 4 * wave_length
 z = np.linspace(0, 1, 1000)
 x = np.linspace(0, x_max, 200)
-psi_1d = structure_function_normal(U0, U1, k, omega, z, N)
+psi_1d = structure_function_normal(nu, U0, U1, k, omega, z, N)
 psi =  np.real(np.outer(psi_1d, np.exp(1j * k * x)))
 ax3 = fig.add_subplot(133)
 ax3.set_title(r"$k={:.1f}, \ \sigma={:.1f}$".format(k, omega),
