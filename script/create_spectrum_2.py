4a8b83fc74793232b043e4a29b6a5bff7096ea63
Yohei Onuki <onuki@riam.kyushu-u.ac.jp>
2025-01-14 14:56:42 +0900

    Update create_spectrum_2.py

--- script/create_spectrum_2.py
+++ script/create_spectrum_2.py
@@ -10,40 +10,30 @@ fs = 20
 eps_i = 1.e-10j
 
 
-def Bessel_I(nu, z, N):
+def Bessel_I(mu, z, N):
     I = 1.e0
     eps = np.sign(np.real(z)) * eps_i
     for m in range(1, N):
         a = 1
         for j in range(1, m+1):
-            a = a * (z + eps)**2 / (4 * j * (j + nu))            
+            a = a * (z + eps)**2 / (4 * j * (j + mu))            
         I = I + a
-    I = (z / 2 + eps)**nu / gamma(nu + 1) * I
+    I = (z / 2 + eps)**mu / gamma(mu + 1) * I
     return I
 
 
-def II(nu, z1, z2, N):
-    return (Bessel_I(nu, z1, N)
-            * Bessel_I(-nu, z2, N)
-            - Bessel_I(-nu, z1, N)
-            * Bessel_I(nu, z2, N))
-
-
-def D_func(U0, U1, k, omega, N):
-    nu = 1j * (1 / (U1 - U0)**2 - 0.25)**0.5
-    Z0 = (k * U0 - omega) / (U1 - U0)
-    Z1 = (k * U1 - omega) / (U1 - U0)
-    return np.sqrt(Z1 * Z0 + eps_i) * (Bessel_I(- nu, Z1, N)
-            * Bessel_I(nu, Z0, N)
-            - Bessel_I(nu, Z1, N)
-            * Bessel_I(-nu, Z0, N))
+def II(mu, z1, z2, N):
+    return (Bessel_I(mu, z1, N)
+            * Bessel_I(-mu, z2, N)
+            - Bessel_I(-mu, z1, N)
+            * Bessel_I(mu, z2, N))
 
 
 buoyancy_frequency = 1.
 U0 = 0.05
-U1 = 0.15
-Ri = buoyancy_frequency / (U0 - U1)**2
-nu = 1j * (Ri - 0.25)**0.5
+U1 = 0.2
+Ri = buoyancy_frequency**2 / (U0 - U1)**2
+nu = (Ri - 0.25)**0.5
 
 N = 100
 NK = 100
@@ -66,7 +56,7 @@ for j in range(2*NK+1):
 Z0 = (k * U0 - omega) / (U1 - U0)
 Z1 = (k * U1 - omega) / (U1 - U0)
 
-func = (1j * II(nu, Z0, Z1, N))
+func = (1j * II(1j * nu, Z0, Z1, N))
 
 fig = plt.figure(figsize=(8, 4))
 ax1 = fig.add_subplot(111)
